cd63497abb0c85b572528688e267bdb3bd82b2c6
Rob Johansen <robert_johans22@hotmail.com>
2020-04-04 17:33:59 -0600

    Do a little project cleanup and organization

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import store from './app/store'
+import App from './ui/pages/home/App'
+import store from './ui/store/store'
 import { Provider } from 'react-redux'
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,8 +11,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
