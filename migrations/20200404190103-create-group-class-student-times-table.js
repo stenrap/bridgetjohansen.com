44c4ffcdb85ca6bb013f4774f5ce3adbd0e8220c
Rob Johansen <robert_johans22@hotmail.com>
2020-04-04 13:18:27 -0600

    Add a working migration for creating the group_class_student_times table

---
+++ migrations/20200404190103-create-group-class-student-times-table.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const logError = (err) => {
+  if (err) console.log(`Error in ${__filename} migration:`, err)
+}
+
+exports.up = async db => {
+  try {
+    await db.createTable('group_class_student_times', {
+      id: {
+        type: 'INTEGER',
+        primaryKey: true,
+        notNull: true,
+        autoIncrement: true
+      },
+      student_id: {
+        type: 'INTEGER',
+        notNull: true,
+        foreignKey: {
+          name: 'group_class_student_times_student_fk',
+          table: 'students',
+          mapping: 'id',
+          rules: {
+            onDelete: 'CASCADE'
+          }
+        }
+      },
+      group_class_time_id: {
+        type: 'INTEGER',
+        notNull: true,
+        foreignKey: {
+          name: 'group_class_student_times_time_fk',
+          table: 'group_class_times',
+          mapping: 'id',
+          rules: {
+            onDelete: 'CASCADE'
+          }
+        }
+      }
+    })
+  } catch (err) {
+    logError(err)
+  }
+}
+
+exports.down = async db => {
+  return db.dropTable('group_class_student_times')
+}
+
+exports._meta = {
+  version: 1
+}
