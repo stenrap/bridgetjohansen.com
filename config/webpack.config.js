a9c7c3c91b18d626e806587702ea8b4ddf129226
Rob Johansen <robert_johans22@hotmail.com>
2020-04-04 20:53:52 -0600

    Move the setupTests.js file into the ui directory

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -641,7 +641,7 @@ module.exports = function (webpackEnv) {
             '!**/__tests__/**',
             '!**/?(*.)(spec|test).*',
             '!**/src/setupProxy.*',
-            '!**/src/setupTests.*'
+            '!**/src/ui/setupTests.*'
           ],
           silent: true,
           // The formatter is invoked directly in WebpackDevServerUtils during development
