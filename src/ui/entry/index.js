57febfd96950474874a05a6b5ea01efcf85571fa
Rob Johansen <robert_johans22@hotmail.com>
2020-04-04 20:50:38 -0600

    Move the ui entry file to a better location

--- src/ui/entry/index.js
+++ src/ui/entry/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './ui/pages/home/App'
-import store from './ui/store/store'
+import App from '../pages/home/App'
+import store from '../store/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.render(
