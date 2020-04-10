1db6ab9acf9842afdca4022fe407f32d6a48fb5f
Rob Johansen <robert_johans22@hotmail.com>
2020-04-09 22:38:32 -0600

    Add a working "Sign in with Google" button to the /sign-in page

--- src/ui/pages/sign-in/SignIn.js
+++ src/ui/pages/sign-in/SignIn.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import styles from './SignIn.module.scss'
 
 export default () => {
+  const buttonBoxId = 'googleButtonBoxId'
+
+  useEffect(() => {
+    window.gapi.signin2.render(buttonBoxId, {
+      height: 46,
+      longtitle: true,
+      onSuccess: googleUser => {
+        console.log(`Logged in as: ${googleUser.getBasicProfile().getEmail()}`)
+        console.log(`The id token to send to the back end is: ${googleUser.getAuthResponse().id_token}`)
+      },
+      scope: 'email',
+      width: 217
+    })
+  }, [])
+
   return (
-    <div>Hello, sign-in!</div>
+    <div
+      className={styles.googleButtonBox}
+      id={buttonBoxId}
+    />
   )
 }
