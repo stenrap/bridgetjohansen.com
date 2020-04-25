bbd5fe4bfef695a1ab59f82368a0230800c0bfe0
Rob Johansen <robert_johans22@hotmail.com>
2020-04-25 17:41:00 -0600

    Add a calendar component and most the style overrides for it

--- src/shared/libs/format.js
+++ src/shared/libs/format.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const MONTHS = [
   'January',
   'February',
--- src/ui/components/schedule-date-link/ScheduleDateLink.js
+++ src/ui/components/schedule-date-link/ScheduleDateLink.js
@@ -1,16 +1,33 @@
 import React, { useState } from 'react'
+import 'react-calendar/dist/Calendar.css'
 
+import Calendar from 'react-calendar'
 import Modal from '../modal/Modal'
 import styles from './ScheduleDateLink.module.scss'
 
 export default ({ date }) => {
   const [modalOpen, setModalOpen] = useState(false)
+  const [calendarDate, setCalendarDate] = useState(new Date())
 
   const modal = modalOpen && (
     <Modal
-      title='Lesson Schedule Effective Date'
+      title='Schedule Effective Date'
     >
-      Hello, modal!
+      <Calendar
+        calendarType='US'
+        className={styles.calendar}
+        minDate={new Date()}
+        minDetail='month'
+        next2Label={null}
+        onChange={date => {
+          setCalendarDate(date)
+        }}
+        prev2Label={null}
+        showFixedNumberOfWeeks
+        tileClassName={styles.calendarDay}
+        value={calendarDate}
+        view='month'
+      />
     </Modal>
   )
 
