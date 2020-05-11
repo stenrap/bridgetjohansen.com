bf0b6c55febaf179d8ec211505e9224f47618eb3
Rob Johansen <robert_johans22@hotmail.com>
2020-05-10 20:41:52 -0600

    A bit more progress on the student modal

--- src/ui/components/modal/StudentModal.js
+++ src/ui/components/modal/StudentModal.js
@@ -10,10 +10,11 @@ export default props => {
     student = {}
   } = props
   // const [emails, setEmails] = useState(student.emails || [])
+  const [lessonAmPm, setLessonAmPm] = useState(student.lessonAmPm || 'pm')
   const [lessonDay, setLessonDay] = useState(student.lessonDay || TUESDAY)
   // const [lessonDuration, setLessonDuration] = useState(student.lessonDuration || '')
-  // const [lessonHour, setLessonHour] = useState(student.lessonHour || '')
-  // const [lessonMinutes, setLessonMinutes] = useState(student.lessonMinutes || '')
+  const [lessonHour, setLessonHour] = useState(student.lessonHour || 2)
+  const [lessonMinutes, setLessonMinutes] = useState(student.lessonMinutes || 0)
   const [name, setName] = useState(student.name || '')
   const [parents, setParents] = useState(student.parents || '')
   const [phone, setPhone] = useState(student.phone || '')
@@ -21,6 +22,7 @@ export default props => {
   return (
     <Modal
       {...props}
+      className={styles.studentModal}
       title={`${student.id ? 'Edit' : 'Add'} Student`}
     >
       <div className={styles.inputRow}>
@@ -49,19 +51,65 @@ export default props => {
       </div>
       <div className={styles.inputRow}>
         <label>Lesson</label>
-        <select
-          className={styles.lessonDay}
-          onChange={event => setLessonDay(event.target.value)}
-          value={lessonDay}
-        >
-          <option value={SUNDAY}>Sunday</option>
-          <option value={MONDAY}>Monday</option>
-          <option value={TUESDAY}>Tuesday</option>
-          <option value={WEDNESDAY}>Wednesday</option>
-          <option value={THURSDAY}>Thursday</option>
-          <option value={FRIDAY}>Friday</option>
-          <option value={SATURDAY}>Saturday</option>
-        </select>
+        <div className={styles.lessonSelects}>
+          <select
+            className={styles.lessonDay}
+            onChange={event => setLessonDay(event.target.value)}
+            value={lessonDay}
+          >
+            <option value={SUNDAY}>Sunday</option>
+            <option value={MONDAY}>Monday</option>
+            <option value={TUESDAY}>Tuesday</option>
+            <option value={WEDNESDAY}>Wednesday</option>
+            <option value={THURSDAY}>Thursday</option>
+            <option value={FRIDAY}>Friday</option>
+            <option value={SATURDAY}>Saturday</option>
+          </select>
+          <select
+            className={styles.lessonHour}
+            onChange={event => setLessonHour(event.target.value)}
+            value={lessonHour}
+          >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={6}>6</option>
+            <option value={7}>7</option>
+            <option value={8}>8</option>
+            <option value={9}>9</option>
+            <option value={10}>10</option>
+            <option value={11}>11</option>
+            <option value={12}>12</option>
+          </select>
+          <select
+            className={styles.lessonMinutes}
+            onChange={event => setLessonMinutes(event.target.value)}
+            value={lessonMinutes}
+          >
+            <option value={0}>00</option>
+            <option value={5}>05</option>
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={20}>20</option>
+            <option value={25}>25</option>
+            <option value={30}>30</option>
+            <option value={35}>35</option>
+            <option value={40}>40</option>
+            <option value={45}>45</option>
+            <option value={50}>50</option>
+            <option value={55}>55</option>
+          </select>
+          <select
+            className={styles.lessonMinutes}
+            onChange={event => setLessonAmPm(event.target.value)}
+            value={lessonAmPm}
+          >
+            <option value='am'>AM</option>
+            <option value='pm'>PM</option>
+          </select>
+        </div>
       </div>
     </Modal>
   )
