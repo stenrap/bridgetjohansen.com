1ecd888fdf4d49f0a0715fdb03e328140ef443ec
Rob Johansen <robert_johans22@hotmail.com>
2020-05-23 11:59:06 -0600

    Improve the layout and size of the student modal.
    Closes #30.

--- src/ui/components/modal/StudentModal.js
+++ src/ui/components/modal/StudentModal.js
@@ -36,6 +36,7 @@ export default props => {
   } else {
     return (
       <Modal
+        className={styles.studentModal}
         {...props}
         onOk={() => {
           if (!isValidString(name)) setNameError(true)
@@ -97,80 +98,72 @@ export default props => {
           />
         </div>
         <div className={styles.inputRow}>
-          <label>Lesson Day</label>
+          <label>Lesson</label>
           <select
             className={styles.lessonDay}
             onChange={event => setLessonDay(Number(event.target.value))}
             value={lessonDay}
           >
-            <option value={SUNDAY}>Sunday</option>
-            <option value={MONDAY}>Monday</option>
-            <option value={TUESDAY}>Tuesday</option>
-            <option value={WEDNESDAY}>Wednesday</option>
-            <option value={THURSDAY}>Thursday</option>
-            <option value={FRIDAY}>Friday</option>
-            <option value={SATURDAY}>Saturday</option>
+            <option value={SUNDAY}>Sun</option>
+            <option value={MONDAY}>Mon</option>
+            <option value={TUESDAY}>Tue</option>
+            <option value={WEDNESDAY}>Wed</option>
+            <option value={THURSDAY}>Thu</option>
+            <option value={FRIDAY}>Fri</option>
+            <option value={SATURDAY}>Sat</option>
           </select>
-        </div>
-        <div className={styles.inputRow}>
-          <label>Lesson Time</label>
-          <div className={styles.lessonSelects}>
-            <select
-              className={styles.lessonHour}
-              onChange={event => setLessonHour(Number(event.target.value))}
-              value={lessonHour}
-            >
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={4}>4</option>
-              <option value={5}>5</option>
-              <option value={6}>6</option>
-              <option value={7}>7</option>
-              <option value={8}>8</option>
-              <option value={9}>9</option>
-              <option value={10}>10</option>
-              <option value={11}>11</option>
-              <option value={12}>12</option>
-            </select>
-            <select
-              className={styles.lessonMinutes}
-              onChange={event => setLessonMinutes(Number(event.target.value))}
-              value={lessonMinutes}
-            >
-              <option value={0}>00</option>
-              <option value={5}>05</option>
-              <option value={10}>10</option>
-              <option value={15}>15</option>
-              <option value={20}>20</option>
-              <option value={25}>25</option>
-              <option value={30}>30</option>
-              <option value={35}>35</option>
-              <option value={40}>40</option>
-              <option value={45}>45</option>
-              <option value={50}>50</option>
-              <option value={55}>55</option>
-            </select>
-            <select
-              className={styles.lessonMinutes}
-              onChange={event => setLessonMeridiem(event.target.value)}
-              value={lessonMeridiem}
-            >
-              <option value='am'>AM</option>
-              <option value='pm'>PM</option>
-            </select>
-          </div>
-        </div>
-        <div className={styles.inputRow}>
-          <label>Lesson Duration</label>
+          <select
+            className={styles.lessonHour}
+            onChange={event => setLessonHour(Number(event.target.value))}
+            value={lessonHour}
+          >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={6}>6</option>
+            <option value={7}>7</option>
+            <option value={8}>8</option>
+            <option value={9}>9</option>
+            <option value={10}>10</option>
+            <option value={11}>11</option>
+            <option value={12}>12</option>
+          </select>
+          <select
+            className={styles.lessonMinutes}
+            onChange={event => setLessonMinutes(Number(event.target.value))}
+            value={lessonMinutes}
+          >
+            <option value={0}>00</option>
+            <option value={5}>05</option>
+            <option value={10}>10</option>
+            <option value={15}>15</option>
+            <option value={20}>20</option>
+            <option value={25}>25</option>
+            <option value={30}>30</option>
+            <option value={35}>35</option>
+            <option value={40}>40</option>
+            <option value={45}>45</option>
+            <option value={50}>50</option>
+            <option value={55}>55</option>
+          </select>
+          <select
+            className={styles.lessonMinutes}
+            onChange={event => setLessonMeridiem(event.target.value)}
+            value={lessonMeridiem}
+          >
+            <option value='am'>AM</option>
+            <option value='pm'>PM</option>
+          </select>
           <select
             className={styles.lessonDuration}
             onChange={event => setLessonDuration(Number(event.target.value))}
             value={lessonDuration}
           >
-            <option value={30}>30 minutes</option>
-            <option value={45}>45 minutes</option>
-            <option value={60}>60 minutes</option>
+            <option value={30}>30 min</option>
+            <option value={45}>45 min</option>
+            <option value={60}>60 min</option>
           </select>
         </div>
         <div className={styles.inputRow}>
