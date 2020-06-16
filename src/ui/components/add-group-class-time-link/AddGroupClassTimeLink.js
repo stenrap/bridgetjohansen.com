760f2c2176ff77e7ae268648a50ada8adc916228
Rob Johansen <robert_johans22@hotmail.com>
2020-06-16 17:13:32 -0600

    Show error modal on trying to add a group class time when there are no students/parents

--- src/ui/components/add-group-class-time-link/AddGroupClassTimeLink.js
+++ src/ui/components/add-group-class-time-link/AddGroupClassTimeLink.js
@@ -1,16 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { getParents, getStudents, setAddingParent, setAddingStudent } from '../../store/scheduleSlice'
+import Modal from '../modals/Modal'
 import styles from './AddGroupClassTimeLink.module.scss'
 
 export default () => {
+  const dispatch = useDispatch()
+  const parents = useSelector(getParents)
+  const students = useSelector(getStudents)
+
+  const [noParents, setNoParents] = useState(false)
+  const [noStudents, setNoStudents] = useState(false)
+
+  const noParentsModal = noParents && (
+    <Modal
+      className={styles.missingSomethingModal}
+      onOk={() => {
+        setNoParents(false)
+        dispatch(setAddingParent(true))
+      }}
+      showCancel={false}
+      title='Need Students'
+    >
+      <p className={styles.missingSomethingText}>
+        You need parents and students before you can add any
+        group class times. Please start by adding a parent.
+      </p>
+    </Modal>
+  )
+
+  const noStudentsModal = noStudents && (
+    <Modal
+      className={styles.missingSomethingModal}
+      onOk={() => {
+        setNoStudents(false)
+        dispatch(setAddingStudent(true))
+      }}
+      showCancel={false}
+      title='Need Students'
+    >
+      <p className={styles.missingSomethingText}>
+        Adding a group class time requires selecting students from a
+        list, but you have no students. Please add a student first.
+      </p>
+    </Modal>
+  )
+
   return (
     <>
       <span
         className={styles.addTimeLink}
-        onClick={() => console.log('Opening the Add Time modal...')}
+        onClick={() => {
+          if (parents.length === 0) return setNoParents(true)
+          if (students.length === 0) return setNoStudents(true)
+        }}
       >
         Add Group Class Time
       </span>
+      {noParentsModal}
+      {noStudentsModal}
     </>
   )
 }
