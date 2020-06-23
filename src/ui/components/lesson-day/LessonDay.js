998d38917b13b7e8aaeda6e0bd6636a2878d61fe
Rob Johansen <robert_johans22@hotmail.com>
2020-06-22 19:24:40 -0600

    Simplify the logic for rendering the LessonDay component

--- src/ui/components/lesson-day/LessonDay.js
+++ src/ui/components/lesson-day/LessonDay.js
@@ -4,21 +4,15 @@ import Student from '../student/Student'
 import styles from './LessonDay.module.scss'
 
 export default props => {
-  const students = []
-
-  for (const student of props.students) {
-    students.push(
-      <Student key={`student-${student.id}`} {...student} />
-    )
-  }
-
   return (
     <div className={styles.day}>
       <div className={styles.name}>
         {props.name}
       </div>
       <div className={styles.students}>
-        {students}
+        {props.students.map(student => {
+          return <Student key={`student-${student.id}`} {...student} />
+        })}
       </div>
     </div>
   )
