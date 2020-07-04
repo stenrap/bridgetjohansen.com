04205daaebf9730abb0d39ee23426033dc542048
Rob Johansen <robert_johans22@hotmail.com>
2020-07-04 08:14:37 -0600

    Add the events banner and header

--- src/ui/pages/events/Events.js
+++ src/ui/pages/events/Events.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import styles from './Events.module.scss'
+
 export default () => {
   return (
-    <div>Hello, events!</div>
+    <>
+      <div className={styles.banner} />
+      <div className={styles.events}>
+        <div className={styles.eventsHeader}>
+          Events
+        </div>
+      </div>
+    </>
   )
 }
