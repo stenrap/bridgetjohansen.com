cca444dd3f3f48a94368c0c95c806b50518ba51b
Rob Johansen <robert_johans22@hotmail.com>
2020-07-14 20:05:16 -0600

    Wrestle the Steinway into submission

--- src/ui/pages/home/Home.js
+++ src/ui/pages/home/Home.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import styles from './Home.module.scss'
 
 export default () => {
+  useEffect(() => {
+    document.body.style.backgroundColor = '#010101'
+    return () => {
+      document.body.style.backgroundColor = 'unset'
+    }
+  })
+
   return (
-    <div>Hello, home!</div>
+    <picture className={styles.background}>
+      <source
+        media='(orientation: portrait) and (min-width: 834px)'
+        srcSet='/images/steinway-portrait-huge.jpg'
+      />
+      <source
+        media='(orientation: portrait) and (min-width: 768px)'
+        srcSet='/images/steinway-portrait-large.jpg'
+      />
+      <source
+        media='(orientation: portrait) and (min-width: 411px)'
+        srcSet='/images/steinway-portrait-medium.jpg'
+      />
+      <source
+        media='(orientation: landscape) and (min-width: 1920px)'
+        srcSet='/images/steinway-landscape-huge.jpg'
+      />
+      <source
+        media='(orientation: landscape) and (min-width: 1024px)'
+        srcSet='/images/steinway-landscape-large.jpg'
+      />
+      <source
+        media='(orientation: landscape) and (min-width: 711px)'
+        srcSet='/images/steinway-landscape-medium.jpg'
+      />
+      <source
+        media='(orientation: landscape) and (min-width: 640px)'
+        srcSet='/images/steinway-landscape-small.jpg'
+      />
+      <img alt='Steinway' src='/images/steinway-portrait-small.jpg' />
+    </picture>
   )
 }
