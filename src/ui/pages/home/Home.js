50f3c4a4a1be6cc15fba2d2d6eb98f87a0c6ab3c
Rob Johansen <robert_johans22@hotmail.com>
2020-07-15 19:10:23 -0600

    Add the quote with various breakpoints

--- src/ui/pages/home/Home.js
+++ src/ui/pages/home/Home.js
@@ -11,36 +11,47 @@ export default () => {
   })
 
   return (
-    <picture className={styles.background}>
-      <source
-        media='(orientation: portrait) and (min-width: 834px)'
-        srcSet='/images/steinway-portrait-huge.jpg'
-      />
-      <source
-        media='(orientation: portrait) and (min-width: 768px)'
-        srcSet='/images/steinway-portrait-large.jpg'
-      />
-      <source
-        media='(orientation: portrait) and (min-width: 411px)'
-        srcSet='/images/steinway-portrait-medium.jpg'
-      />
-      <source
-        media='(orientation: landscape) and (min-width: 1920px)'
-        srcSet='/images/steinway-landscape-huge.jpg'
-      />
-      <source
-        media='(orientation: landscape) and (min-width: 1024px)'
-        srcSet='/images/steinway-landscape-large.jpg'
-      />
-      <source
-        media='(orientation: landscape) and (min-width: 711px)'
-        srcSet='/images/steinway-landscape-medium.jpg'
-      />
-      <source
-        media='(orientation: landscape) and (min-width: 640px)'
-        srcSet='/images/steinway-landscape-small.jpg'
-      />
-      <img alt='Steinway' src='/images/steinway-portrait-small.jpg' />
-    </picture>
+    <>
+      <div className={styles.quoteBox}>
+        <div className={styles.quote}>
+          The final aim and reason of all music is nothing other than the
+          glorification of God and the refreshment of the human spirit.
+        </div>
+        <div className={styles.bach}>
+          Johann Sebastian Bach
+        </div>
+      </div>
+      <picture className={styles.background}>
+        <source
+          media='(orientation: portrait) and (min-width: 834px)'
+          srcSet='/images/steinway-portrait-huge.jpg'
+        />
+        <source
+          media='(orientation: portrait) and (min-width: 768px)'
+          srcSet='/images/steinway-portrait-large.jpg'
+        />
+        <source
+          media='(orientation: portrait) and (min-width: 411px)'
+          srcSet='/images/steinway-portrait-medium.jpg'
+        />
+        <source
+          media='(orientation: landscape) and (min-width: 1920px)'
+          srcSet='/images/steinway-landscape-huge.jpg'
+        />
+        <source
+          media='(orientation: landscape) and (min-width: 1024px)'
+          srcSet='/images/steinway-landscape-large.jpg'
+        />
+        <source
+          media='(orientation: landscape) and (min-width: 711px)'
+          srcSet='/images/steinway-landscape-medium.jpg'
+        />
+        <source
+          media='(orientation: landscape) and (min-width: 640px)'
+          srcSet='/images/steinway-landscape-small.jpg'
+        />
+        <img alt='Steinway' src='/images/steinway-portrait-small.jpg' />
+      </picture>
+    </>
   )
 }
