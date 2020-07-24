f1432ac9abb7e9309e5e5e615da46a3bdeb00398
Rob Johansen <robert_johans22@hotmail.com>
2020-07-23 19:33:00 -0600

    Begin fleshing out the About page

--- src/ui/pages/about/About.js
+++ src/ui/pages/about/About.js
@@ -1,7 +1,47 @@
 import React from 'react'
 
+import styles from './About.module.scss'
+
+const STARTING_YEAR = 1989
+
 export default () => {
+  const today = new Date()
+  const total = today.getFullYear() - STARTING_YEAR
+
   return (
-    <div>Hello, about!</div>
+    <div className={styles.about}>
+      <div className={styles.bridget}>
+        <img alt='Bridget Johansen' className={styles.bridgetPhoto} src='/images/bridget.jpg' />
+        <div className={styles.explanation}>
+          <p>
+            Bridget McBride Johansen, Nationally Certified Teacher of Music (NCTM), has offered private
+            piano instruction for {total} years.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+      <div className={styles.usu}>
+        <img alt='Utah State University' className={styles.usuPhoto} src='/images/usu.jpg' />
+        <div className={styles.explanation}>
+          <p>
+            Bridget earned a bachelor's degree in piano pedagogy from Utah State University.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
