ba702fe2cbecda2fb611761138b80555c08555d3
Rob Johansen <rob.johansen@gmail.com>
2021-04-03 09:15:02 -0600

    Add support for validating email locally before checking it with the back end

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,9 +1,17 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FocusEvent, useState } from 'react'
 import Head from 'next/head'
 
 import { AppDispatch } from '../store/store'
-import { isEmailAvailable } from '../store/userSlice'
-import { useAppDispatch } from '../store/hooks'
+import {
+  checkEmail,
+  getEmailError,
+  isCheckingEmail,
+  isEmailAvailable,
+  setEmailAvailable,
+  setEmailError
+} from '../store/userSlice'
+import { useAppDispatch, useAppSelector } from '../store/hooks'
+import { validateEmail } from '../shared/validations/user/user'
 import Input from '../ui/components/input/Input'
 import Nav from '../ui/components/nav/Nav'
 import styles from '../ui/styles/pages/CreateAccount.module.scss'
@@ -11,8 +19,10 @@ import styles from '../ui/styles/pages/CreateAccount.module.scss'
 const CreateAccount = (): JSX.Element => {
   const dispatch: AppDispatch = useAppDispatch()
 
+  const emailAvailable: boolean = useAppSelector(isEmailAvailable)
+
   const [email, setEmail] = useState('')
-  // const [emailError, setEmailError] = useState('')
+  const emailError: string = useAppSelector(getEmailError)
 
   const [firstName, setFirstName] = useState('')
   // const [firstNameError, setFirstNameError] = useState('')
@@ -25,15 +35,35 @@ const CreateAccount = (): JSX.Element => {
   const [password, setPassword] = useState('')
   // const [passwordError, setPasswordError] = useState('')
 
-  const checkEmail = (): void => {
-    // TODO and WYLO 1 .... You should only dispatch if you haven't already checked whether the email is available.
-    //                      So you'll need a global state property for whether the email is available.
-    //                      You'll also eventually need to disable the 'Next' button (or the form submit--should this be a form?) while checking the email
-    dispatch(isEmailAvailable(email))
+  const onBlurEmail = (event: FocusEvent<HTMLInputElement>): void => {
+    const candidateEmail: string = event.target.value
+    if (candidateEmail.length === 0) return
+
+    try {
+      validateEmail(candidateEmail)
+    } catch (err) {
+      dispatch(setEmailError(err.message))
+      return
+    }
+
+    if (!emailAvailable) {
+      dispatch(checkEmail(candidateEmail))
+    }
+  }
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>): void => {
+    dispatch(setEmailAvailable(false))
+    dispatch(setEmailError(''))
+    setEmail(event.target.value)
+  }
+
+  if (emailError) {
+    console.log(emailError)
   }
 
   /*
     TODO and WYLO ....
+      1. Update the Input component to show a tooltip on error, then pass `emailError` into the email input
       2. Put the below form into its own component (maybe CreateAccountStart)
       3. Implement a CreateAccountVerify component that accepts the 6-digit code for email verification
       4. Wire everything up and create the user!
@@ -49,7 +79,7 @@ const CreateAccount = (): JSX.Element => {
         <h1 className='pageHeader'>Create your account</h1>
         <Input onChange={(event): void => setFirstName(event.target.value)} placeholder='First name' type='text' value={firstName} />
         <Input onChange={(event): void => setLastName(event.target.value)} placeholder='Last name' type='text' value={lastName} />
-        <Input onBlur={checkEmail} onChange={(event: ChangeEvent<HTMLInputElement>): void => setEmail(event.target.value)} placeholder='Email' type='email' value={email} />
+        <Input onBlur={onBlurEmail} onChange={onChangeEmail} placeholder='Email' type='email' value={email} />
         <Input onChange={(event): void => setPassword(event.target.value)} placeholder='Password' type='password' value={password} />
       </div>
     </>
--- store/userSlice.ts
+++ store/userSlice.ts
@@ -1,3 +1,4 @@
+import { batch } from 'react-redux'
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 import * as requests from '../ui/requests'
@@ -5,8 +6,10 @@ import { AppThunk, AppThunkDispatch, RootState } from './store'
 
 export interface UserState {
   admin: boolean
-  checkingIsEmailAvailable: boolean
+  checkingEmail: boolean
   email: string
+  emailAvailable: boolean
+  emailError: string
   firstName: string
   id: number
   lastName: string
@@ -16,8 +19,10 @@ export interface UserState {
 
 const initialState: UserState = {
   admin: false,
-  checkingIsEmailAvailable: false,
+  checkingEmail: false,
   email: '',
+  emailAvailable: false,
+  emailError: '',
   firstName: '',
   id: 0,
   lastName: '',
@@ -29,8 +34,14 @@ export const slice = createSlice({
   initialState,
   name: 'user',
   reducers: {
-    setCheckingIsEmailAvailable: (state: UserState, action: PayloadAction<boolean>): void => {
-      state.checkingIsEmailAvailable = action.payload
+    setCheckingEmail: (state: UserState, action: PayloadAction<boolean>): void => {
+      state.checkingEmail = action.payload
+    },
+    setEmailAvailable: (state: UserState, action: PayloadAction<boolean>): void => {
+      state.emailAvailable = action.payload
+    },
+    setEmailError: (state: UserState, action: PayloadAction<string>): void => {
+      state.emailError = action.payload
     },
     setUser: (state: UserState, action: PayloadAction<UserState>): void => {
       state.admin = action.payload.admin
@@ -41,26 +52,34 @@ export const slice = createSlice({
 })
 
 // Actions
-export const { setCheckingIsEmailAvailable, setUser } = slice.actions
+export const { setCheckingEmail, setEmailAvailable, setEmailError, setUser } = slice.actions
 
 // Thunks
-export const isEmailAvailable = (email: string): AppThunk => async (dispatch: AppThunkDispatch): Promise<void> => {
-  dispatch(setCheckingIsEmailAvailable(true))
+export const checkEmail = (email: string): AppThunk => async (dispatch: AppThunkDispatch): Promise<void> => {
+  dispatch(setCheckingEmail(true))
 
   const response: requests.IsEmailAvailableResponse = await requests.isEmailAvailable(email)
 
   if (response.errors) {
     console.log('Error checking whether email is available:', response.errors[0].message)
-    return
+    return batch((): void => {
+      dispatch(setEmailAvailable(false))
+      dispatch(setCheckingEmail(false))
+    })
   }
 
-  console.log('response.data?.isEmailAvailable is:', response.data?.isEmailAvailable)
-
-  dispatch(setCheckingIsEmailAvailable(false))
+  batch((): void => {
+    console.log('response.data?.isEmailAvailable is:', response.data?.isEmailAvailable)
+    dispatch(setEmailAvailable(Boolean(response.data?.isEmailAvailable)))
+    dispatch(setCheckingEmail(false))
+  })
 }
 
 // Selectors
+export const getEmailError = (state: RootState): string => state.user.emailError
 export const isAdmin = (state: RootState): boolean => state.user.admin
+export const isCheckingEmail = (state: RootState): boolean => state.user.checkingEmail
+export const isEmailAvailable = (state: RootState): boolean => state.user.emailAvailable
 
 // Reducer
 export default slice.reducer
